8738235d1d70c57bd845b0bdd7bd0dc5f7c9cb60
divakaivan <timee98@outlook.com>
2025-05-31 22:44:47 +0900

    done

--- exercises/100_for4.zig
+++ exercises/100_for4.zig
@@ -36,10 +36,14 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() void {
-    const hex_nums = [_]u8{ 0xb, 0x2a, 0x77 };
+    const hex_nums = [_]u8{
+        0xb,
+        0x2a,
+        0x77,
+    };
     const dec_nums = [_]u8{ 11, 42, 119 };
 
-    for (hex_nums, ???) |hn, ???| {
+    for (hex_nums, dec_nums) |hn, dn| {
         if (hn != dn) {
             print("Uh oh! Found a mismatch: {d} vs {d}\n", .{ hn, dn });
             return;
